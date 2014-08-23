61a334cb301d9b19a6c5f33a48c5ed2adce457be
Adam Streck <adam.streck@gmail.com>
2014-08-23 18:40:28 +0200

    Now triggering collisions and pausing.

--- Assets/Character/movement.cs
+++ Assets/Character/movement.cs
@@ -6,10 +6,14 @@ public class Movement : MonoBehaviour {
 	protected float move_force = 1f;
 	protected float jump_force = 150f;
 	protected Transform ground_check;			// A position marking where to check if the player is grounded.
-	protected bool grounded = false;			// Whether or not the player is grounded.
+	protected bool ground_A = false;			// Whether hits the Actual ground
+	protected bool ground_B = false;			// Whether hits the Book ground
 	protected bool pressed = false;
 
 	protected Animator animator;
+
+	protected enum World {Actual, Book};
+	protected World current_world; // In which world the character currently is
 
 	void Awake()
 	{
@@ -22,12 +26,18 @@ public class Movement : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		current_world = World.Actual;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		transform.rotation = Quaternion.identity;
+		ground_A = Physics2D.Linecast(transform.position, ground_check.position, 1 << LayerMask.NameToLayer("Ground_A"));
+		ground_B = Physics2D.Linecast(transform.position, ground_check.position, 1 << LayerMask.NameToLayer("Ground_B"));
+		if ((ground_A && current_world == World.Book) || (ground_B && current_world == World.Actual)) {
+			Debug.Log("Crash");
+			Time.timeScale = 0;
+		}
 	}
 
 	void FixedUpdate() {
@@ -41,12 +51,10 @@ public class Movement : MonoBehaviour {
 			// ... set the player's velocity to the maxSpeed in the x axis.
 			rigidbody2D.velocity = new Vector2(Mathf.Sign(rigidbody2D.velocity.x) * max_speed, rigidbody2D.velocity.y);
 
-		Debug.Log(rigidbody2D.velocity.y);
-		grounded = Physics2D.Linecast(transform.position, ground_check.position, 1 << LayerMask.NameToLayer("Ground"));
-
 		animator.SetBool("Jump", false);
 		animator.SetBool("Switch", false);
 		if (Input.GetButtonDown("Jump")) {
+			current_world = (World) System.Convert.ToInt32(!System.Convert.ToBoolean(current_world));
 			rigidbody2D.AddForce(new Vector2(0f, jump_force));
 			animator.SetBool("Jump", true);
 		}
@@ -60,5 +68,13 @@ public class Movement : MonoBehaviour {
 					rigidbody2D.AddForce(new Vector2(0f, rigidbody2D.velocity.y * 0.02f * jump_force));
 			}
 		} */
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if ((other.gameObject.tag == "GroundA" && current_world == World.Book) 
+		    || (other.gameObject.tag == "GroundB" && current_world == World.Actual)) {
+			Debug.Log("Crash");
+			Time.timeScale = 0;
+		}
 	}
 }
